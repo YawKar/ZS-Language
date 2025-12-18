91097347119a5cf117df4fb4542518df983af3ee
Зарина Шарипова <Sh.zarina.ild@gmail.com>
2025-12-18 04:06:21 +0300

    Сделала аргументы в Middle

--- Middle-End/main.cpp
+++ Middle-End/main.cpp
@@ -2,11 +2,18 @@
 #include "Structs.h"
 #include "Enums.h"
 #include "Front-End/LanguageFunctions.h"
-#include "TreeToCode.h"
+#include "Reverse-End/TreeToCode.h"
 #include "DoGraph.h"
 #include "Middle-End/Optimise.h"
 
-int main(void) {
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        fprintf(stderr, "Usage: %s <tree_file>\n", argv[0]);
+        return 1;
+    }
+
+    char *tree_file = argv[1];
+    
     LangRoot root = {};
     LangRootCtor(&root);
 
@@ -17,29 +24,33 @@ int main(void) {
     INIT_DUMP_INFO(dump_info);
     dump_info.tree = &root;
     Language lang_info = {&root, NULL, NULL, &Variable_Array};
-    CHECK_ERROR_RETURN(ReadInfix(&lang_info, &dump_info, "input.txt"));
+
+    FILE_OPEN_AND_CHECK(ast_file_read, tree_file, "r");
     
-    FILE_OPEN_AND_CHECK(ast_file_read, "ast.txt", "r");
     FileInfo info = {};
-    DoBufRead(ast_file_read, "ast.txt", &info);
+    DoBufRead(ast_file_read, tree_file, &info);
+    fclose(ast_file_read);
 
     LangRoot root1 = {};
     LangRootCtor(&root1);
-    LangNode_t new_node = {};
-    LangNode_t *new_node_adr = &new_node;
+    
+    LangNode_t *root_node = NULL;
     size_t pos = 0;
-    ParseNodeFromString(info.buf_ptr, &pos, root.root, &new_node_adr, &Variable_Array);
-    root1.root = new_node_adr;
+    CHECK_ERROR_RETURN(ParseNodeFromString(info.buf_ptr, &pos, NULL, &root_node, &Variable_Array));
+    root1.root = root_node;
+    
     dump_info.tree = &root1;
     DoTreeInGraphviz(root1.root, &dump_info, &Variable_Array);
+
     root1.root = OptimiseTree(&root1, root1.root, &Variable_Array);
-    printf("AAAAAAAAAAAA");
-    PrintAST(root1.root, ast_file_read, &Variable_Array, 0);
-    DoTreeInGraphviz(root1.root, &dump_info, &Variable_Array);
     
+    FILE_OPEN_AND_CHECK(ast_file_write, tree_file, "w");
+    PrintAST(root1.root, ast_file_write, &Variable_Array, 0);
+    fclose(ast_file_write);
+    
+    DoTreeInGraphviz(root1.root, &dump_info, &Variable_Array);
+
     DtorVariableArray(&Variable_Array);
-    fclose(ast_file_read);
-    //TreeDtor(&root1);
 
     return kSuccess;
 }
